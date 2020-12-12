bd7785efec953d7ebb403e40c0afe23444bed947
alicewild11 <55978056+alicewild11@users.noreply.github.com>
2020-12-12 16:36:05 +0000

    Title

--- ksb.php
+++ ksb.php
@@ -19,5 +19,18 @@ if ($result->num_rows > 0) {
 }
 
 $link->close();
-
  ?>
+
+ <!DOCTYPE html>
+ <html lang="en">
+ <head>
+     <meta charset="UTF-8">
+     <title>Welcome</title>
+     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.css">
+     <style type="text/css">
+         body{ font: 14px sans-serif; text-align: center; }
+     </style>
+ </head>
+ <body>
+ </body>
+ </html>
